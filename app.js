635a5615143716e1628cd925ef5d4fa65bc3d059
huynhquynh-dev <huynhquynh-dev>
2022-01-26 15:55:11 +0700

    - Add router

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const { post } = require('./router/post');
 
 // Khởi động app
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.json())
 
 // Kết nối CSDL
 connectDB()
+
+app.use('/posts', post)
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
