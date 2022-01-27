6160f8dd37d10e7477d785d1afbdb1d6b5452b4d
huynhquynh-dev <huynhquynh-dev>
2022-01-27 10:34:38 +0700

    - Add giao dien

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const exphbs = require('express-handlebars')
+
+// Nhap khau routes
 const { post } = require('./router/post');
 
 // Khởi động app
 const app = express();
+
+// Khoi dong Handlebars middleware
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
 //  Khởi động express middleware
 app.use(express.json())
@@ -11,6 +18,11 @@ app.use(express.json())
 // Kết nối CSDL
 connectDB()
 
+// Mot so routes co ban, co the dua vao file rieng trong thu muc routes
+app.get('/', (req, res) => res.render('index'))
+app.get('/about', (req, res) => res.render('about'))
+
+// Mang routes vao de su dung
 app.use('/posts', post)
 
 const PORT = 5000
